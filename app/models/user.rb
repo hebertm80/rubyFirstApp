62cf098c6714ac60bc27595ea26d8cd3af5eb1be
Marc-André Hébert <hebert.marc-andre@courrier.uqam.ca>
2011-12-27 21:56:39 -0500

    Add controller pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,44 @@
 
 require 'digest/sha2'
 class User < ActiveRecord::Base
-   validates :name, :length => {:maximum => 10}
+
    attr_accessor :password
+
+   validates :name, :length => {:maximum => 10}
+
 
    validates_presence_of :password, :if => :password_required?
    validates_length_of :password, :within => 4..40, :if => :password_required?
 
-   before_save encrypt_new_password
+   before_save :encrypt_new_password
 
-   def encrypt_new_password
-      return if password.blank?
-      self.password = encrypt password
+  # @param login [String]
+  # @param password [String]
+  def self.authenticate(login, password)
+      user = self.find_by_name(login)
+      return user if user && user.authenticated(password)
+    end
+
+  # @param password [string]
+  def authenticated(password)
+     hashed_password == encrypt(password)
    end
 
-  def password_required?
-    password.blank? || !password.
-  end
+   protected
+
+     def encrypt_new_password
+        return if password.blank?
+        self.hashed_password = encrypt password
+     end
+
+      def password_required?
+        hashed_password.blank? || !password.blank?
+      end
+
+      def encrypt(string)
+        Digest::SHA2::base64digest(string)
+      end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FirstApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   resources :users
 
   # The priority is based upon order of creation:
@@ -57,4 +61,6 @@ FirstApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  root :to => 'pages#home'
 end
